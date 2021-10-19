3027ffb89cb66f7f41003791232bcde635cb0b32
Mehak-Chib <84865152+Mehak-Chib@users.noreply.github.com>
2021-10-19 15:18:03 +0530

    Add files via upload

---
+++ basic datatype.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b ;
+    char c;
+    float d;
+    double e;
+    
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<"\n"<<b<<"\n"<<c<<endl;
+    
+    cout.precision(3);
+    cout<<fixed<<d<<endl;
+    
+    cout.precision(9);
+    cout<<fixed<<e<<endl;
+    
+    return 0;
+}
